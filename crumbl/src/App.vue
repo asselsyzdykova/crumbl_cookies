8e95d8675ae28dad4bfd43bc3d93c7b043f7f03c
asselsyzdykova <161850628+asselsyzdykova@users.noreply.github.com>
2025-01-22 00:11:59 +0100

    Create App.vue

---
+++ crumbl/src/App.vue
@@ -0,0 +1,45 @@
+<template>
+  <div id="app">
+    <AppHeader />
+    <main>
+      <router-view />
+    </main>
+    <CartSidebar v-if="cartStore.isCartVisible" />
+    <AppFooter />
+  </div>
+</template>
+
+<script>
+import AppHeader from "@/components/AppHeader.vue";
+import AppFooter from "@/components/AppFooter.vue";
+import CartSidebar from "@/components/CartSidebar.vue";
+import { useCartStore } from "@/stores/cartStore";
+
+export default {
+  name: "App",
+  components: {
+    AppHeader,
+    AppFooter,
+    CartSidebar,
+  },
+  data() {
+    return {
+      cartStore: useCartStore(),
+    };
+  },
+  mounted() {
+    this.cartStore.loadCartFromLocalStorage();
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Arial, sans-serif;
+  text-align: center;
+  color: #333;
+}
+main {
+  padding: 1rem;
+}
+</style>
