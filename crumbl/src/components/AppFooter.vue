86dd57b73d08337ea531866068a35d8c81a14179
asselsyzdykova <161850628+asselsyzdykova@users.noreply.github.com>
2025-01-22 00:13:44 +0100

    Create AppFooter.vue

---
+++ crumbl/src/components/AppFooter.vue
@@ -0,0 +1,28 @@
+<template>
+    <footer>
+      <p>&copy; 2025 Crumbl Cookies. All rights reserved.</p>
+    </footer>
+  </template>
+
+  <script>
+  export default {
+    name: "AppFooter",
+  };
+  </script>
+
+<style>
+footer {
+  margin-top: 20px;
+  padding: 1rem;
+  background-color: #f8bbd0;
+  color: #880e4f;
+  margin: 0;
+  width: 100%;
+  box-sizing: border-box;
+}
+
+body {
+  margin: 0;
+}
+</style>
+
